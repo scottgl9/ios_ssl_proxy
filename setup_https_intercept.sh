1293d559e86b11160f7b10474571ecfd1ec1bc18
Scott Glover <scott.glover@r1soft.com>
2016-11-23 00:50:25 -0600

    Added certs.

--- setup_https_intercept.sh
+++ setup_https_intercept.sh
@@ -3,4 +3,5 @@
 openssl genrsa -out ca.key 2048
 openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=proxy2 CA"
 openssl genrsa -out cert.key 2048
+cat ca.key ca.crt > ca.cer
 mkdir certs/
