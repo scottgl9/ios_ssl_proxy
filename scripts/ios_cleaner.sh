72e9ba7ca0f85bf49c917a7471be73f796895252
¨Scott <scott.glover@r1soft.com>
2017-01-24 10:51:45 -0600

    Added my ios cleaner script.

---
+++ scripts/ios_cleaner.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+rm -f /var/mobile/Library/Accounts/*.sqlite*
+rm -f /var/mobile/Library/IdentityServices/ids*
+rm -f /var/mobile/Library/ApplePushService/aps.*
+rm -f /var/mobile/Library/Preferences/com.apple.ids.*
+rm -f /var/mobile/Library/Preferences/com.apple.icloud.*
+rm -f /var/mobile/Library/Preferences/com.apple.accountsd.plist
+rm -rf /var/mobile/Library/Caches/com.apple.*
+rm -rf /var/mobile/Library/DataAccess/*
