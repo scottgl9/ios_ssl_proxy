a61367479eaaaa3507dffd46fbd96d70c359d095
¨Scott <scott.glover@r1soft.com>
2017-02-03 13:13:42 -0600

    Updates to ios_cleaner script.

--- scripts/ios_cleaner.sh
+++ scripts/ios_cleaner.sh
@@ -1,35 +1,47 @@
 #!/bin/sh
 
+killall -9 akd apsd pkd findmydeviced fmfd fmflocatord identityservicesd seld
 rm -f /var/Keychains/*.db*
 rm -f /var/Keychains/*.sqlite*
-rm -f /var/Keychains/keychain-ota-backup.plist
-rm -rf /var/root/Library/Caches/*
 rm -f /var/root/Library/Preferences/com.apple.coreservices.appleidauthenticationinfo.plist
 rm -f /var/mobile/Library/AccountNotifications/*.sqlite*
 rm -f /var/mobile/Library/Accounts/*.sqlite*
 rm -f /var/mobile/Library/AddressBook/*.sqlite*
+# delete ccertain cache files only
 find /var/mobile/Library/Caches -name "Cache.db*"  -exec rm {} \;
+rm -rf /var/mobile/Library/Caches/CloudKit/*
+rm -f /var/mobile/Library/Caches/FamilyCircle/*
+rm -rf /var/mobile/Library/Caches/com.apple.accountsd/*
+rm -rf /var/mobile/Library/Caches/com.apple.akd/*
+rm -rf var/mobile/Library/Caches/com.apple.dataaccess.dataaccessd/*
+rm -rf /var/mobile/Library/Caches/com.apple.imfoundation.IMRemoteURLConnectionAgent/*
+rm -rf /var/mobile/Library/Caches/com.apple.Preferences/*
+rm -rf /var/mobile/Library/Caches/com.apple.purplebuddy/*
+rm -rf /var/mobile/Library/Caches/PassKit/*
+
+rm -f /var/mobile/Library/ApplePushService/aps.*
 rm -rf /var/mobile/Library/DataAccess/*
 rm -f /var/mobile/Library/Health/*.sqlite*
 rm -f /var/mobile/Library/IdentityServices/ids*
 rm -f /var/mobile/Library/ApplePushService/aps.*
-rm -f /var/mobile/Library/Preferences/com.apple.accounts*.plist
-rm -f /var/mobile/Library/Preferences/com.apple.assistant*.plist
-rm -f /var/mobile/Library/Preferences/com.apple.CoreDuet*.plist
+rm -f /var/mobile/Library/Preferences/com.apple.accounts*
+rm -f /var/mobile/Library/Preferences/com.apple.assistant*
+rm -f /var/mobile/Library/Preferences/com.apple.CoreDuet*
 rm -f /var/mobile/Library/Preferences/com.apple.gamed*.plist
 rm -f /var/mobile/Library/Preferences/com.apple.identityservices*
 rm -f /var/mobile/Library/Preferences/com.apple.imservice*
 rm -f /var/mobile/Library/Preferences/com.apple.ids.*
 rm -f /var/mobile/Library/Preferences/com.apple.icloud.*
-rm -f /var/mobile/Library/Preferences/com.apple.mobile.ldpair.plist
-rm -f /var/mobile/Library/Preferences/com.apple.seld.plist
+rm -f /var/mobile/Library/Preferences/com.apple.mobile.*
+rm -f /var/mobile/Library/Preferences/com.apple.seld.*
 rm -f /var/mobile/Library/Preferences/ProtectedCloudKeySyncing.plist
 rm -f /var/mobile/Library/SyncedPreferences/com.apple.cloudrecents.*
 rm -f /var/mobile/Library/SyncedPreferences/com.apple.cmfsyncagent.plist
 rm -f /var/mobile/Library/SyncedPreferences/com.apple.sbd*
 rm -f /var/mobile/Library/SyncedPreferences/com.apple.security.cloudkeychainproxy3.plist
 rm -f /var/mobile/Library/SyncedPreferences/com.apple.syncedpreferences.plist
-rm -rf /var/mobile/Library/Caches/com.apple.*
+#rm -rf /var/mobile/Library/Caches/com.apple.*
 rm -f /var/preferences/com.apple.security.cloudkeychainproxy3.keysToRegister.plist
-rm -f /var/tmp/com.apple.purplebuddy.sentinel
-rm -rf /var/tmp/*
+killall -9 akd apsd pkd findmydeviced fmfd fmflocatord identityservicesd seld
+# Respring
+killall -9 backboardd SpringBoard
