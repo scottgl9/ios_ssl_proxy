63295eac5e2587ea04bad95110ce654712cf2fab
¨Scott <scott.glover@r1soft.com>
2017-03-07 16:13:14 -0600

    Added itunes_hash_rename script.

---
+++ itunes/itunes_hash_rename.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+# script to rename files from an iOS itunes backup to their correct names
+
+import sys
+import os
+
+if sys.argv[2:]:
+    hashmap = sys.argv[1]
+    path = sys.argv[2]
+
+else:
+    print("Usage: %s <hashmap> <backup path>" % sys.argv[0])
+    exit(0)
+
+    
+def traverse(a, hashdict):
+    for item in os.listdir(a):
+        fullpath=os.path.join(a,item)
+        if os.path.isfile(fullpath) and item in hashdict:
+            print("%s -> %s" % (item, hashdict[item]))
+        elif os.path.isdir(fullpath):
+            traverse(fullpath, hashdict)
+
+def load_hashmap(path):
+    hashdict = dict()
+    with open(path) as file_:
+        for line in file_:
+            line = line.rstrip('\n')
+            parts = line.split(' ')
+            hashdict[parts[0]] = parts[1]
+    return hashdict
+
+hashdict = load_hashmap(hashmap)
+print(hashdict)
+traverse(path, hashdict)
