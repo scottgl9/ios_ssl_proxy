24a1de43c753e2a034d5cf3a0a61c2e7033fbfdd
¨Scott <scott.glover@r1soft.com>
2017-05-08 16:49:24 -0500

    Updates to certTableTool including updates to use Python 3.

--- scripts/certTableTool.py
+++ scripts/certTableTool.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python3
 # used to unpack certsTable.data
 # The certsIndex.data file is a Database Index file that contains an array which can be read using NSData,
 # this contains a list of sha1 hashes and offsets. 
@@ -8,8 +8,9 @@ import binascii
 import struct
 import sys
 import os
-from OpenSSL import crypto, SSL
+#from OpenSSL import crypto, SSL
 import hashlib
+import plistlib
 
 if sys.argv[1:]:
         cmdtype = sys.argv[1]
@@ -17,25 +18,29 @@ else:
     print("Usage: %s unpack|pack" % sys.argv[0])
     exit(0)
 
+def file_sha256(filepath):
+    with open(filepath, 'rb') as f:
+        return hashlib.sha256(f.read()).digest()
+
 def extract_certs(filename):
-	count=0
-	st_key=bytearray(open(filename, 'rb').read())
-        #dirname = ("%s_extracted" % os.path.splitext(os.path.basename(filename))[0])
-        os.mkdir(dirname, 0755 )
+        count=0
+        st_key=bytearray(open(filename, 'rb').read())
+        dirname = ("%s" % os.path.splitext(os.path.basename(filename))[0])
+        os.mkdir(dirname)
 
         while 1: 
-		index = st_key.find("\x30\x82")
-		if index < 0: break
-		length = struct.unpack(">h", st_key[index+2:index+4])[0] + 5
-                if length > len(st_key):
-                    print("Length of %d extends past end" % length)
-                    return
-		print("index=%d, length=%d" % (index, length))
-		certdata = st_key[index:index+length]
-		with open(os.path.join(dirname, "%d.cer" % count), "wb") as f:
-			f.write(certdata)
-                count = count + 1
-		st_key = st_key[index+length:]
+            index = st_key.find(b"\x30\x82")
+            if index < 0: break
+            length = struct.unpack(">h", st_key[index+2:index+4])[0] + 5
+            if length > len(st_key):
+                print("Length of %d extends past end" % length)
+                return
+            print("index=%d, length=%d" % (index, length))
+            certdata = st_key[index:index+length]
+            with open(os.path.join(dirname, "%d.cer" % count), "wb") as f:
+                f.write(certdata)
+            count = count + 1
+            st_key = st_key[index+length:]
 
 # block_length determines the offset to the next 8 bytes before the cert.
 # index_length is the actual length of the certificate, and if index_length < block_length,
@@ -45,10 +50,10 @@ def unpack_certTable(filename):
         index=0
         count=0
         dirname = "certsTable" #% os.path.splitext(os.path.basename(filename))[0])
-        #os.mkdir(dirname, 0755 )
+        #os.mkdir(dirname)
 
         while index < len(st_key):
-                index = st_key.find("\x30\x82", index)
+                index = st_key.find(b"\x30\x82", index)
                 if index < 0: break
                 clength = struct.unpack(">h", st_key[index+2:index+4])[0] + 5
                 ilength = struct.unpack("<I", st_key[index-4:index])[0]
@@ -58,9 +63,9 @@ def unpack_certTable(filename):
                 if calclength != blength: print("%x != %x" % (calclength, blength))
                 print("index=%d, index_length=%x, block_length=%x, calclength=%x" % (index, ilength, blength, calclength))
                 certdata = st_key[index:index+ilength]
-                cert=crypto.load_certificate(crypto.FILETYPE_ASN1, bytes(certdata))
-                certhash = hashlib.sha1(crypto.dump_publickey(crypto.FILETYPE_ASN1, cert.get_pubkey())).hexdigest()
-                print(certhash)
+                #cert=crypto.load_certificate(crypto.FILETYPE_ASN1, bytes(certdata))
+                #certhash = hashlib.sha1(crypto.dump_publickey(crypto.FILETYPE_ASN1, cert.get_pubkey())).hexdigest()
+                #print(certhash)
                 #print(cert.digest('sha1').replace(':',''))
                 #print(cert.get_subject())
                 with open(os.path.join(dirname, "%d.cer" % count), "wb") as f:
@@ -68,10 +73,10 @@ def unpack_certTable(filename):
                 count = count + 1
                 index = index + clength
 
-def pack_certTable(path):
+def pack_certTable(path, filename):
        index=0
        count=0
-       outf = open("certsTable.data.new", "wb")
+       outf = open(filename, "wb")
 
        while 1:
            filepath = os.path.join(path, "%d.cer" % count)
@@ -84,14 +89,14 @@ def pack_certTable(path):
                outf.write(struct.pack("<I", calclength))
                outf.write(struct.pack("<I", ilength))
                outf.write(certdata)
-               outf.write("\xFF" * (calclength - ilength - 8))
+               outf.write(b"\xFF" * (calclength - ilength - 8))
            else: break
            count = count + 1
        outf.close()
 
 def unpack_indexTable(filename, path):
     inf = open(filename, 'rb')
-    outf = open("certsTable/certsIndex.txt", "wb")
+    outf = open("certsTable/certsIndex.txt", "wt")
     count=0
     while 1:
         hashdata = inf.read(20)
@@ -99,11 +104,11 @@ def unpack_indexTable(filename, path):
         if len(hashdata) <= 0: return
         index = struct.unpack("<I", inf.read(4))[0] + 8
         filepath = os.path.join(path, "%d.cer" % count)
-        outf.write("%s %s %d\n" % (filepath, binascii.hexlify(hashdata), index))
+        outf.write(str("%s %s %d\n" % (filepath, binascii.hexlify(hashdata).decode("utf-8"), index)))
         count=count + 1
 
-def pack_indexTable(path):
-    outf = open("certsIndex.data.new", "wb")
+def pack_indexTable(path, filename):
+    outf = open(filename, "wb")
     with open('certsTable/certsIndex.txt') as f:
         for line in f:
             parts = line.strip().split(' ')
@@ -120,5 +125,11 @@ if cmdtype == 'unpack':
     unpack_indexTable("certsIndex.data", "certsTable")
 elif cmdtype == 'pack':
     print("Packing certsTable directory to certsTable.data...")
-    #pack_certTable("./certsTable")
-    pack_indexTable("certsTable")
+    pack_certTable("./certsTable", "certsTable.data.new")
+    pack_indexTable("certsTable", "certsIndex.data.new")
+    allowed=plistlib.load(open("Allowed.plist", 'rb'), fmt=plistlib.FMT_BINARY)
+    print(str(allowed['65F231AD2AF7F7DD52960AC702C10EEFA6D53B11']))
+    pm=plistlib.load(open("manifest.data", 'rb'), fmt=plistlib.FMT_BINARY)
+    pm['certsIndex.data'] = file_sha256("certsIndex.data")
+    pm['certsTable.data'] = file_sha256("certsTable.data")
+    plistlib.dump(pm, open("manifest.data.new", 'wb'), fmt=plistlib.FMT_BINARY)
