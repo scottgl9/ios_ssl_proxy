f679254a6c5f6003ea559e03d18ca988a54f3ed4
¨Scott <scott.glover@r1soft.com>
2017-05-23 12:25:54 -0500

    Updated test.py.

--- test.py
+++ test.py
@@ -2,23 +2,32 @@
 import socket
 import ssl
 from OpenSSL import SSL
+from twisted.application import internet, service
+from twisted.python import log
+from twisted.spread import pb
+
 #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #s = ssl.wrap_socket(s, ca_certs="server_certs/APNCertChain.crt", cert_reqs=ssl.CERT_REQUIRED)
 #s.connect(("17.249.60.13", 443))
-TLSv1_2_METHOD = 6
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ctx = SSL.Context(SSL.TLSv1_2_METHOD)
-ctx.load_verify_locations("APNCertChain.crt")
 s.connect(("17.249.60.13", 5223))
+ctx = SSL.Context(SSL.TLSv1_2_METHOD)
+ctx.set_options(SSL.OP_NO_TLSv1)
+ctx.use_certificate_chain_file("APNCertChain.crt")
+ctx.set_alpn_protos([b'apns-security-v2'])
 sock = SSL.Connection(ctx, s)
 sock.set_tlsext_host_name("courier.push.apple.com")
+sock.set_alpn_protos([b'apns-security-v2'])
 sock.set_connect_state()
-sock.do_handshake()
-#context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
-#context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-#context.set_alpn_protocols(["apns-security-v2"])
-#context.options |= ssl.OP_NO_TLSv1 | ssl.OP_TLS_BLOCK_PADDING_BUG
-#context.verify_mode = ssl.CERT_REQUIRED
-#context.load_verify_locations(capath="APNCertChain.crt")
-#s = context.wrap_socket(s, server_hostname='courier.push.apple.com')
-#s.connect(("17.249.60.13", 5223))
+#sock.connect(("17.249.60.13", 5223))
+while True:
+    try:
+        sock.do_handshake()
+    except SSL.WantReadError:
+        select.select([s], [], [])
+        continue
+    except SSL.Error as e:
+        raise ssl.SSLError('bad handshake %v', e)
+    break
+sslsock = WrappedSocket(ctx, s)
+
