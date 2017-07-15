b77367c73c621dc94fdbea70d261020bd1454fdb
Scott Glover <scott.glover@r1soft.com>
2017-07-14 19:10:23 -0500

    Updates to create_device_tweak.py.

--- scripts/create_device_tweak.py
+++ scripts/create_device_tweak.py
@@ -10,9 +10,9 @@ import plistlib
 import datetime
 
 # scp com.nablac0d3.SSLKillSwitchSettings.plist root@192.168.0.144:/var/mobile/Library/Preferences/com.nablac0d3.SSLKillSwitchSettings.plist
-key_list = ["BuildVersion", "DeviceColor", "DeviceEnclosureColor", "HardwareModel", "ModelNumber", "ProductType", "ProductVersion", "SerialNumber", "UniqueDeviceID", "WiFiAddress", "DieId", "EnclosureColor", "HardwareModel", "HardwarePlatform", "BluetoothAddress", "EthernetAddress", "UniqueChipID", "DieID", "MLBSerialNumber", "FirmwareVersion", "CPUArchitecture", "WirelessBoardSnum", "BasebandCertId", "BasebandChipID", "BasebandMasterKeyHash", "BasebandVersion", "BoardId", "InternationalMobileEquipmentIdentity", "MobileEquipmentIdentifier", "WirelessBoardSerialNumber"]
+key_list = ["BuildVersion", "DeviceColor", "DeviceEnclosureColor", "HardwareModel", "MarketingName", "ModelNumber", "ProductType", "ProductVersion", "SerialNumber", "UniqueDeviceID", "WiFiAddress", "DieId", "EnclosureColor", "HardwareModel", "HardwarePlatform", "BluetoothAddress", "EthernetAddress", "UniqueChipID", "DieID", "MLBSerialNumber", "FirmwareVersion", "CPUArchitecture", "WirelessBoardSnum", "BasebandCertId", "BasebandChipID", "BasebandKeyHashInformation", "BasebandMasterKeyHash", "BasebandSerialNumber", "BasebandVersion", "BasebandRegionSKU", "BoardId", "InternationalMobileEquipmentIdentity", "MobileEquipmentIdentifier", "WirelessBoardSerialNumber", "RegulatoryModelNumber", "PkHash", "BasebandFirmwareManifestData", "ChipID", "ChipSerialNo", "CertID", "BasebandRegionSKU"]
 
-#, "IntegratedCircuitCardIdentity", "CarrierBundleInfoArray"]
+#"IntegratedCircuitCardIdentity", "CarrierBundleInfoArray"]
 
 def load_device_info(sn):
     if '.xml' in sn:
@@ -34,8 +34,12 @@ for key in devinfo:
     if key in key_list:
         if key == "HardwareModel":
             p['HWModelStr'] = devinfo[key]
+            p[key] = devinfo[key]
+        elif key == "MarketingName":
+            p['marketing-name'] = devinfo[key]
         elif key == "BasebandVersion":
             p['BasebandFirmwareVersion'] = devinfo[key]
+            p[key] = devinfo[key]
         elif key == "WiFiAddress":
             p['WifiAddress'] = devinfo[key]
         elif key == "EthernetAddress":
@@ -51,6 +55,9 @@ for key in devinfo:
         print("%s = %s" % (key, devinfo[key]))
 
 p['UniqueDeviceIDData'] = plistlib.Data(binascii.unhexlify(p['UniqueDeviceID']))
+p['mac-address-wifi0'] = plistlib.Data(binascii.unhexlify(p['WifiAddress'].replace(':','')))
+p['mac-address-bluetooth0'] = plistlib.Data(binascii.unhexlify(p['BluetoothAddress'].replace(':','')))
+#p['IOMACAddress'] = p['mac-address-wifi0']
 #udid = ~hex(devinfo['UniqueDeviceID'])
 #print(udid)
 plistlib.writePlist(p, "com.nablac0d3.SSLKillSwitchSettings.plist")
